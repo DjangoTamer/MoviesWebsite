6592df3f52c90b9db90938b039a49adb448f566e
Slava_work <dyuminslava155@gmail.com>
2022-09-20 17:07:05 +0300

    rev_1.8 authentication_and_registration

--- proj_movies/proj_movies/urls.py
+++ proj_movies/proj_movies/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('subscription/', include('app_mail_subscription.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('app_movies.urls'))
 ]
 
